0216b45db55311217a90cbea1b8e2a642622c3de
Fredrick Burns <fredburns@runitapps.com>
2019-10-22 00:01:22 -0500

    Added Base Classes for UITabBar Integration

---
+++ SamsClubDemo/SamsClubDemo/Views/ProductListCell.swift
@@ -0,0 +1,14 @@
+//
+//  ProductListCell.swift
+//  SamsClubDemo
+//
+//  Created by Fredrick Burns on 10/21/19.
+//  Copyright © 2019 Fredrick Burns. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ProductListCell: UICollectionViewCell {
+    
+}
---
+++ SamsClubDemo/SamsClubDemo/Views/SavedItemsCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SavedItemsCollectionViewCell.swift
+//  SamsClubDemo
+//
+//  Created by Fredrick Burns on 10/21/19.
+//  Copyright © 2019 Fredrick Burns. All rights reserved.
+//
+
+import UIKit
+
+class SavedItemsCollectionViewCell: UICollectionViewCell {
+    
+}
