5f8c9018aa0a11ce7de8f0ff147e62de4f0bc326
Fredrick Burns <fredburns@runitapps.com>
2019-10-23 13:17:17 -0500

    Added an extension to Help with UIObject Set up

---
+++ SamsClubDemo/SamsClubDemo/Helpers/Extensions.swift
@@ -0,0 +1,37 @@
+//
+//  Extensions.swift
+//  SamsClubDemo
+//
+//  Created by Fredrick Burns on 10/23/19.
+//  Copyright © 2019 Fredrick Burns. All rights reserved.
+//
+
+import UIKit
+
+extension UILabel {
+    convenience init(text: String, font: UIFont, numberOfLines: Int = 1, textColor: UIColor = .black, adjustToFitWith: Bool = true, adjustFontContentCat: Bool = true) {
+        self.init(frame: .zero)
+        self.text = text
+        self.font = font
+        self.numberOfLines = 2
+        self.textColor = .black
+        self.adjustsFontForContentSizeCategory = true
+        self.adjustsFontForContentSizeCategory = true
+    }
+}
+
+extension UIImageView {
+    convenience init(cornerRadius: CGFloat) {
+        self.init(image: nil)
+        self.layer.cornerRadius = cornerRadius
+        self.clipsToBounds = true
+        self.contentMode = .scaleAspectFill
+    }
+}
+
+extension UIButton {
+    convenience init(title: String) {
+        self.init(type: .system)
+        self.setTitle(title, for: .normal)
+    }
+}
