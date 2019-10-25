feb829dfdb62bc2eab4d08e3a1ed6b50f0d2a6d4
Fredrick Burns <fredburns@runitapps.com>
2019-10-24 23:18:43 -0500

    Imported Cosmos Pod for User Ratings

--- SamsClubDemo/Pods/Target Support Files/Pods-SamsClubDemo/Pods-SamsClubDemo-frameworks.sh
+++ SamsClubDemo/Pods/Target Support Files/Pods-SamsClubDemo/Pods-SamsClubDemo-frameworks.sh
@@ -161,10 +161,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Cosmos/Cosmos.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftSoup/SwiftSoup.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Cosmos/Cosmos.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftSoup/SwiftSoup.framework"
 fi
